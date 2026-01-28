7c7993bba0c698df44ab95d51d10fccf3a6eebeb
ROUNAK VATS <rounakvats525@gmail.com>
2026-01-28 11:20:13 +0530

    Add courseinfo (Part 1 exercises 1.1–1.5)

---
+++ part1/src/App.jsx
@@ -0,0 +1,52 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </div>
+  )
+}
+
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ]
+
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total total={total} />
+    </div>
+  )
+}
+
+export default App
