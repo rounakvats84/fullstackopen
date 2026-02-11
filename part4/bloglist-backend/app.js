9b0a1d4232ebb18621263ee6aa500ade85fd8b4f
ROUNAK VATS <rounakvats525@gmail.com>
2026-02-11 11:47:37 +0530

    Part4 done

---
+++ part4/bloglist-backend/app.js
@@ -0,0 +1,23 @@
+const config = require('./utils/config')
+const express = require('express')
+const mongoose = require('mongoose')
+
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
+
+
+
+const app = express()
+app.use(middleware.tokenExtractor)
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+module.exports = app
